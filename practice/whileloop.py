57f49dd9eac4b0beed086d49971ae1b36334fdd3
Akssayy47 <askhaywaghral00@gmail.com>
2026-01-13 14:13:00 +0530

    1 to 50

---
+++ practice/whileloop.py
@@ -0,0 +1,18 @@
+""""1️ Print numbers from 10 to 1
+Output: 10 9 8 7 ... 1"""
+
+"""
+i = 10
+while i >= 1:
+    print(i)
+    i -= 1
+"""
+
+"""
+(2)
+Print all even numbers between 1 and 50
+"""
+i = 2
+while i <= 50:
+    print(i)
+    i += 2
