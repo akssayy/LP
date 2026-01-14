1208a668bbad2a1cee61b7128a5c636b3e440085
Akssayy47 <askhaywaghral00@gmail.com>
2026-01-14 14:47:50 +0530

    continue

--- practice/whileloop.py
+++ practice/whileloop.py
@@ -12,7 +12,23 @@ while i >= 1:
 (2)
 Print all even numbers between 1 and 50
 """
+"""
 i = 2
 while i <= 50:
     print(i)
     i += 2
+"""
+"""
+3️⃣ Sum of first N natural numbers
+
+Input: N = 5
+Output: 15
+"""
+num = int(input("enter a number"))
+i = 1 
+total = 0 
+
+while i <= num:
+    total = i + total
+    i += 1
+print(f"sum is {total}")
