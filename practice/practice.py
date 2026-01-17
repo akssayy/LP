9d5dc8e922ee6fd508d555049973562c42e9f2df
akssayy <askhaywaghral00@gmail.com>
2026-01-18 00:50:22 +0530

    print numbers

--- practice/practice.py
+++ practice/practice.py
@@ -1,10 +1,4 @@
-num = int(input("enter a number"))
+num = int(input("enter a number: ")) 
 
-if num >= 1:
-    print("positive")
-
-elif num <= -1:
-    print("negitive")
-
-else:
-    print("zero")    
+for i in range(0, num):
+    print(i + 1)
