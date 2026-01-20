4801115ef1111ff7295bf18cbc13dd4869241a50
akssayy <waghralakshay@gmail.com>
2026-01-20 23:03:48 +0530

    pop( )

--- set.py
+++ set.py
@@ -1,7 +1,12 @@
-num = {1, 2, 5, 2, 4, 1}
+"""num = {1, 2, 5, 2, 4, 1}
 num.add(7)
 num.remove(5)
 print(num)
 if 4 in num:
-    print("found")
+    print("found")"""
 
+set = {12, 34, 45, 56, 67}
+x = set.pop() 
+
+
+print(f"{set}")
