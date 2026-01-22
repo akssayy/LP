6f6712767fdd3decd70a0e1b64e0a420df3cc6b6
akssayy <waghralakshay@gmail.com>
2026-01-22 14:49:18 +0530

    function

---
+++ Python/function.py
@@ -0,0 +1,19 @@
+"""def welcome():
+    print("Welcom to python")
+
+welcome()"""
+
+
+"""def mul(a,b):
+    print(a * b)
+
+mul(2,4)"""
+    
+a = int(input("enter a number: "))
+def eo(a):
+    if a % 2 == 0:
+        print("even")
+    else:
+        print("odd")
+
+eo(a)
