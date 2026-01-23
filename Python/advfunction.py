2e493326e09e76f992a036bdf1a7a836499724bf
akssayy <waghralakshay@gmail.com>
2026-01-23 11:06:31 +0530

    max in list by function

--- Python/advfunction.py
+++ Python/advfunction.py
@@ -1,4 +1,4 @@
-numbers = [2, 4, 5, 7]
+"""numbers = [2, 4, 5, 7]
 
 def add_numbers(numbers):
     total = 0
@@ -7,4 +7,17 @@ def add_numbers(numbers):
     return total
 
 result = add_numbers(numbers)
+print(result)"""
+
+numbers = [2, 5, 7, 4, 9]
+
+def find_max(numbers):
+    max = numbers[0]
+    for i in numbers:
+        if i > max:
+            max = i
+
+    return max
+
+result = find_max(numbers)
 print(result)
