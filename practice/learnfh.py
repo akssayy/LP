83fd2b200690a498070610e010878c46ccb7225e
akssayy <waghralakshay@gmail.com>
2026-01-29 01:13:25 +0530

    w in file handling

--- practice/learnfh.py
+++ practice/learnfh.py
@@ -1,5 +1,11 @@
-with open("test.txt", "a") as f:
+"""with open("test.txt", "a") as f:
     f.write("\nnow the file has more content")
+
+with open("test.txt") as f:
+    print(f.read())"""
+
+with open("test.txt", "w") as f:
+    f.write("i have deleted the content") 
 
 with open("test.txt") as f:
     print(f.read())
