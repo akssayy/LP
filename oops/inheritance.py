215ff47531a088f691c2f2324baeb2fe019653c7
akssayy <waghralakshay@gmail.com>
2026-02-01 17:33:19 +0530

    inheritance

---
+++ oops/inheritance.py
@@ -0,0 +1,20 @@
+class vechical:
+    def __init__(self, color, model, speed):
+        self.color = color
+        self.model = model
+        self.speed = speed
+
+    def show_type(self):
+        print(self.color, self.model, self.speed)  
+    
+    def show_speed(self):
+        self.speed = 120
+        print(self.speed)
+
+class car(vechical):
+    def drive(self):
+        print("car is running")
+car1 = car("red", "mustang", 120)
+car1.show_type()
+car1.show_speed()
+car1.drive()
