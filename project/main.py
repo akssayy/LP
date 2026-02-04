478d42ee2980d44246b4cf9abc4a3d972d2d785b
akssayy <waghralakshay@gmail.com>
2026-02-04 15:04:10 +0530

    more clean version

--- project/main.py
+++ project/main.py
@@ -1,5 +1,23 @@
 import os
 
+# ---------- CLASS ----------
+class BankAccount:
+    def __init__(self, name, balance=0):
+        self.name = name
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+
+    def show_details(self):
+        print(self.name, "balance:", self.balance)
+
+
+# ---------- LOAD USERS ----------
 users = []
 
 if os.path.exists("bank.txt"):
@@ -12,93 +30,15 @@ if os.path.exists("bank.txt"):
         name = data[0]
         balance = int(data[1])
 
-        users.append([name, balance])
+        users.append(BankAccount(name, balance))
 
     f.close()
-
-print(users)
-
-
-class BankAccount:
-    def __init__(self, name, balance=0):
-        self.name = name
-        self.balance = balance
-
-    def deposit(self, amount):
-        if amount <= 0:
-            print("Invalid amount")
-        
-        else:
-            self.balance += amount
-            print("Deposited: ", amount)
-
-    def withdraw(self, amount):
-        if amount <= 0:
-            print("Invalid amount") 
-
-        elif amount <= self.balance:
-            self.balance -= amount
-            print("Withdrawn: ", amount)
-
-        else:
-            print("Not enough balnance ")
-
-    def show_details(self):
-        print(self.name, "balance", self.balance)
-
-
-
-accounts = []
-if os.path.exists("bank.txt"):
-    f = open("bank.txt", "r")
-    data = f.read().split(",")
-    f.close()
-    
-    name = data[0]
-    balance = int(data[1])
 
 else:
-    name = "Akshay"
-    balance = 1000
-
-acc = BankAccount(name, balance)
-while True:
-    print("\n1 Deposit")
-    print("\n2 Withdraw")
-    print("\n3 Show balance")
-    print("\n4 exit")
-
-    choice = input("choose: ")
-
-    if choice == "1":
-        amt = int(input("Enter amount: "))
-        acc.deposit(amt)
-    
-    elif choice == "2":
-        amt = int(input("Enter amount: "))
-        acc.withdraw(amt)
-
-    elif choice == "3":
-        acc.show_details()
-
-    elif choice == "4":
-        f = open("bank.txt", "w")
-        f.write(acc.name + "," + str(acc.balance))
-        f.close()
-        print("DAta saved")
-        break
-
-    else:
-        print("invalid choice")
-        
+    print("No bank file found")
 
 
-
-
-"""acc2 = BankAccount("Rahul", 500)
-acc2.deposit(200)
-accounts.append(acc1)
-accounts.append(acc2)
-
-for acc in accounts:
-    acc.show_details()"""
+# ---------- SHOW USERS ----------
+print("\nLoaded Accounts:")
+for acc in users:
+    acc.show_details()
