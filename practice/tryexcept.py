d5bfb9dbc9fe601b62c04806b4b7126d4328e487
akssayy <waghralakshay@gmail.com>
2026-02-07 16:18:45 +0530

    try except example

--- practice/tryexcept.py
+++ practice/tryexcept.py
@@ -1,4 +1,33 @@
 try:
+    num1 = int(input("enter a number: "))
+    num2 = int(input("enter second number: "))
+
+    result = num1 / num2
+    print(f"{result}")
+except ValueError:
+    print("enter valid number")
+
+except ZeroDivisionError:
+    print("number can't be zero(0)")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""try:
     num = int(input("Enter a number to divide by 100: "))
     divide = 100 / num
     print(f"{divide}")
@@ -6,4 +35,4 @@ except ValueError:
     print("please enter a number")
 
 except ZeroDivisionError:
-    print("Cannot divide by zero")
+    print("Cannot divide by zero")"""
