12c60fd1a014f5d0a99352991451d9c43e5fe99d
akssayy <waghralakshay@gmail.com>
2026-02-08 16:05:54 +0530

    AiML project update

--- project/main.py
+++ project/main.py
@@ -1,3 +1,4 @@
+import json
 
 # ---------- CLASS ----------
 class BankAccount:
@@ -16,65 +17,67 @@ class BankAccount:
         print(self.name, "balance:", self.balance)
 
 
-# ---------- LOAD USERS ----------
+# ---------- LOAD DATA ----------
+try:
+    f = open("data.json", "r")
+    data = json.load(f)
+    f.close()
+except:
+    data = {}
 
-
-
-# ---------- SHOW USERS ----------
 print("\nLoaded Accounts:")
-for acc in users:
-    acc.show_details()
+for name, bal in data.items():
+    print(name, "balance:", bal)
 
-#-----------CHOOSE ACCOUNT ----------
+# ---------- CHOOSE ACCOUNT ----------
 name = input("\nEnter account name: ")
 
-current = None
-for acc in users:
-    if acc.name == name:
-        current = acc
+if name not in data:
+    print("New account created!")
+    data[name] = 0
+
+current = BankAccount(name, data[name])
+
+# ---------- MENU ----------
+while True:
+    print("\n1. Deposit")
+    print("2. Withdraw")
+    print("3. Show Details")
+    print("4. Exit")
+
+    choice = input("Choose: ")
+
+    if choice == "1":
+        try:
+            amt = int(input("Amount: "))
+            if amt > 0:
+                current.deposit(amt)
+                print("Deposited", amt)
+        except ValueError:
+            print("Numbers only")
+
+    elif choice == "2":
+        try:
+            amt = int(input("Amount: "))
+            if amt > 0:
+                current.withdraw(amt)
+                print("Withdrew", amt)
+        except ValueError:
+            print("Numbers only")
+
+    elif choice == "3":
+        current.show_details()
+
+    elif choice == "4":
+        # SAVE TO JSON
+        data[name] = current.balance
+
+        f = open("data.json", "w")
+        json.dump(data, f)
+        f.close()
+
+        print("Saved!")
         break
 
-if current:
-
-    while True:
-        print("\n1. Deposit")
-        print("2. Withdraw")
-        print("3. Show Details")
-        print("4. Exit")
-
-        choice = input("Choose an option: ")
-
-        if choice == "1":
-            try:
-                amt = int(input("Enter amount to deposit: "))
-                if amt <= 0:
-                    print("Amount must be positive")
-
-                else:
-                    current.deposit(amt)
-                    print("Deposited", amt)
-            except ValueError:
-                print("invalid Amouunt")
-        elif choice == "2":
-            try:
-                 amt = int(input("Enter amount to withdraw: "))
-                 if amt <= 0:
-                    print("Amount must be positive")
-                 else:  
-                    current.withdraw(amt)
-                    print("Withdrew", amt)
-
-            except ValueError:
-                print("Enter numbers only")
-
-        elif choice == "3":
-            current.show_details()
-
-        elif choice == "4":
-            
-            print("Data saved!")
-            break
-
-        else:
-            print("Invalid option. Please try again.")
-    
+    else:
+        print("Invalid choice")
