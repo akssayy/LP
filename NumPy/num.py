d11b2403610543f1bc3b78733178dc9fe2f89d87
akssayy <waghralakshay@gmail.com>
2026-02-08 17:54:54 +0530

    data types

--- NumPy/num.py
+++ NumPy/num.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-arr = np.array([1, 2, 3, 4, 5, 6, 7])
+arr = np.array(['a', 1, 2, 3, 4], dtype= 'i')
 
-print(arr[:4] )
-#print(type(arr))
+print(arr)
+print(arr.dtype)
 #print('numbers of dimesnsions: ',arr.ndim)
 
 
