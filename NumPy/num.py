bd213103fd45d6d959a2cfe4c6704b87ddccc5d2
akssayy <waghralakshay@gmail.com>
2026-02-08 17:18:18 +0530

    slicing in numpy

--- NumPy/num.py
+++ NumPy/num.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-arr = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
 
-print(arr)
-print(type(arr))
-print(arr.ndim)
+print(arr[:4] )
+#print(type(arr))
+#print('numbers of dimesnsions: ',arr.ndim)
 
 
