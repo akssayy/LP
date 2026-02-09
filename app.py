cc71d4aab91ef5fd5ee51df93269797a117e5266
akssayy <waghralakshay@gmail.com>
2026-02-09 18:25:45 +0530

    first flask page checking

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello, this is my first Flask server!"
+
+@app.route("/user")
+def user():
+    data = {
+        "name": "Akshay",
+        "role": "student",
+        "goal": "full-stack developer"
+    }
+    return jsonify(data)
 
 app.run()
