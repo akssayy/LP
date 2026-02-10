b46d7f8de79539f2bd6d5cdeacd7ec9f132c0811
akssayy <waghralakshay@gmail.com>
2026-02-10 12:29:56 +0530

    ..

--- practice/function.py
+++ practice/function.py
@@ -1,21 +1,34 @@
+age = int(input("enter your age: "))
+
+def check_age(age):
+    if age < 18:
+        return "child"
+    
+    elif  age <=59:
+        return "adult"
+    
+    else:
+        return " senior"
+
+a = check_age(age)
+print(a)
+
+
+
+
+
+"""
 x = [5, 6]
 y = x
 y = y + [7]
 print(x)
 print(y)
-
-
-
-
-
-
-
-
-
+"""
 """number = int(input("Enter a number: "))
 
 def square(number):
     return number * number
 
 s= square(number)
-print(s)"""
+print(s)
+"""
