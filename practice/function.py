e6b6fb057a80fa5287c0026500f26c9b571f2b9b
akssayy <waghralakshay@gmail.com>
2026-02-10 12:19:37 +0530

    list working

--- practice/function.py
+++ practice/function.py
@@ -1,7 +1,21 @@
-number = int(input("Enter a number: "))
+x = [5, 6]
+y = x
+y = y + [7]
+print(x)
+print(y)
+
+
+
+
+
+
+
+
+
+"""number = int(input("Enter a number: "))
 
 def square(number):
     return number * number
 
 s= square(number)
-print(s)
+print(s)"""
