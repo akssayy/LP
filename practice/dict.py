47739086a676df64bebf6c5eba1d93adec74834a
akssayy <waghralakshay@gmail.com>
2026-02-10 17:06:50 +0530

    dictttttttttttttttt

--- practice/dict.py
+++ practice/dict.py
@@ -1,16 +1,29 @@
+"""numbers = [1, 2, 3, 4, 5]
+
+def sum_list(numbers):
+    total = 0
+    for i in numbers:
+        total += i
+
+    return  total
+
+print(sum_list(numbers)"""
+
+
 students = [
     {"name": "A", "marks": 55},
     {"name": "B", "marks": 70},
-    {"name": "C", "marks": 40}
+    {"name": "C", "marks": 60},
+    {"name": "D", "marks": 30}
 ]
 
-def check_result(students):
-    passed = []
-    
+
+def check_passed(students):
+    count = []
     for student in students:
         if student["marks"] >= 60:
-            passed.append(student["name"])
+            count.append(student["name"])
 
-    return passed
+    return count
 
-print(check_result(students))
+print(check_passed(students))
