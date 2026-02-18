3e4c2a4391053f1f6d225ab2f2bc0c5733f85c4c
akssayy <waghralakshay@gmail.com>
2026-02-18 23:55:22 +0530

    added more

--- API/add_app.py
+++ API/add_app.py
@@ -4,11 +4,15 @@ app = Flask(__name__)
 
 users = []
 
-@app.route("/power/<int:base>/<int:exp>")
+@app.route("/power/<base>/<exp>")
 def power(base, exp):
+    base = int(base)
+    exp = int(exp)
     if exp < 0 :
         return jsonify({"error": "exp cannot be less than zero"})
     
     return jsonify({"power": base ** exp})
-
+@app.route("/test/<int:x>")
+def test(x):
+    return {"value": x}
 app.run()
