a854957fa9c847b61e6395c41a19bb8bde21a779
akssayy <waghralakshay@gmail.com>
2026-02-20 16:37:19 +0530

    DELETE

--- HTTP/delete.py
+++ HTTP/delete.py
@@ -6,14 +6,14 @@ users = [{"name": "Akshay"},
          {"name": "Waghral"}]
 
 
-@app.route("/users/<int:index>", methods=["DELETE"])
-def delete_user(index):
-    if index < 0 or index >= len(users):
-        return jsonify({"error": "Invalid index"}), 404
-
-    deleted_user = users.pop(index) 
-
-    return jsonify({"user delected": "sucessfully",
-                    "delected": deleted_user}), 200  
+@app.route("/users/<string:value>", methods=["DELETE"])
+def delete_user(value):
+    for user in users:
+        if user["name"] == value:
+            users.remove(user)
+            return jsonify({"message": "Deleted Successfully",
+                            "deleted_user": user}), 200
+        
+    return jsonify({"error": "user not found"}), 404  
 
 app.run(debug=True)
