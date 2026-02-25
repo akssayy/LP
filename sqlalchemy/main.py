2db2ede5b1c7ad42c64d8f48bf7de10a6fb71048
akssayy <waghralakshay@gmail.com>
2026-02-25 12:17:47 +0530

    first database

--- sqlalchemy/main.py
+++ sqlalchemy/main.py
@@ -3,7 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URL"] = "sqlite:///users.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 
-db = SQLAlchemy(app) 
+db = SQLAlchemy(app)
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(100))
+#create database
+with app.app_context():
+    db.create_all()
+
+app.run(debug=True)
