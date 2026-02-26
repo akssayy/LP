8cf5e2a1864e320ea11681db38467aa7ee69d213
akssayy <waghralakshay@gmail.com>
2026-02-27 01:58:11 +0530

    database connectivivty

--- sqlalchemy/main.py
+++ sqlalchemy/main.py
@@ -9,8 +9,7 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
 
-    id = db.Column(db.Integer, primary_key=True)
-
+    id = db.Column(db.Integer, primary_key=True)  
     name = db.Column(db.String(100))
 #create database
 with app.app_context():
