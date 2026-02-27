c70f6221049c4c311bd7d7a8934be32d3fdebf7a
akssayy <waghralakshay@gmail.com>
2026-02-27 23:56:43 +0530

    understanding basics

--- sqlalchemy/app.py
+++ sqlalchemy/app.py
@@ -8,24 +8,39 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
 db = SQLAlchemy(app)
 
 class User(db.Model):
-    id = db.Column(db.INTEGER, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
 
 with app.app_context():
     db.create_all()
 
-@app.route("/users", methods=["POST"])
-def create_user():
+@app.route("/users", methods=["GET"])
+def get_user():
 
-    data = request.json
+    new_user = User.query.all()
 
-    if not data or "name" not in data:
-        return jsonify({"error": "name required"}), 400
+    if not data:
+        return jsonify({"error": "user not found"})
 
-    new_user = user(name=data["name"])
-    db.session.add(new_user)
-    db.session.commit()
-    
-    return jsonify({"message": "user created"}), 200
+    return jsonify(new_user)
+
+
+
+# ...existing code...
+# @app.route("/users", methods=["POST"])
+# def create_user():
+#
+#     data = request.json
+#
+#     if not data or "name" not in data:
+#         return jsonify({"error": "name required"}), 400
+#
+#     new_user = User(name=data["name"])
+#     db.session.add(new_user)
+#     db.session.commit()
+#
+#     return jsonify({"message": "user created"}), 200
+# ...existing code...
+
 
 app.run(debug=True)
