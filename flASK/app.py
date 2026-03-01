5c943cefc4952670ac241c88c97ecdc52cded35e
akssayy <waghralakshay@gmail.com>
2026-03-01 20:43:14 +0530

    FLASK with database

--- flASK/app.py
+++ flASK/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify 
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,6 +6,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 db = SQLAlchemy(app)
+
 
 class User(db.Model):
 
@@ -15,9 +16,10 @@ class User(db.Model):
 
     email = db.Column(db.String(50))
 
-    posts = db.relationship('post')
+    posts = db.relationship('Post', backref='user')
 
-class post(db.Model):
+
+class Post(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
 
@@ -25,35 +27,56 @@ class post(db.Model):
 
     content = db.Column(db.String(500))
 
-    user_id   = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-with app.app_context():
-    db.create_all()
-
-    """user1 = User(
-        name = "Akshay",
-        email = "akshay@email.com"
-    ) 
-
-    db.session.add(user1)
-    db.session.commit()
-
-    post1 = post(
-        title = "First post",
-        content = "world",
-        user_id=1
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('user.id')
     )
 
-    post2 = post(
-        title = "Second Post",
-        content = "World",
-        user_id=1
-    ) 
 
-    db.session.add(post1)
-    db.session.add(post2)
+with app.app_context():
 
-    db.session.commit()"""
+    db.create_all()
+
+    # Insert data ONLY if database empty
+    if User.query.first() is None:
+
+        user1 = User(
+            name="Akshay",
+            email="akshay@email.com"
+        )
+
+        db.session.add(user1)
+        db.session.commit()
+
+        post1 = Post(
+            title="First Post",
+            content="Hello",
+            user_id=user1.id
+        )
+
+        post2 = Post(
+            title="Second Post",
+            content="World",
+            user_id=user1.id
+        )
+
+        db.session.add(post1)
+        db.session.add(post2)
+
+        db.session.commit()
+
+
+    posts = Post.query.all()
+
+    for post in posts:
+
+        print(post.title)
+
+        print(post.user.name)
+
+"""with app.app_context():
+
+    db.create_all()
 
     user = User.query.first()
 
@@ -61,9 +84,13 @@ with app.app_context():
         for post in user.posts:
             print(post.title)
 
-    posts = post.query.all()
+    posts = Post.query.all()
 
     for post in posts:
-        print(post.title, "belongs to", post.user_id)
+
+        print(post.title)
+
+        print(post.user.name)"""
+
 
 app.run()
