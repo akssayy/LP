2d0d539c87b85adc949b0290eab9c22676607d0e
akssayy <waghralakshay@gmail.com>
2026-03-07 19:18:53 +0530

    crated fully working python + database

--- user_api_project/app.py
+++ user_api_project/app.py
@@ -10,5 +10,10 @@ db = SQLAlchemy(app)
 
 import routes 
 
+from models import User
+
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
     app.run(debug=True)
--- user_api_project/routes.py
+++ user_api_project/routes.py
@@ -2,3 +2,68 @@ from flask import request, jsonify
 from app import app, db
 from models import User
 from schemas import user_schema, users_schema
+
+@app.route("/users", methods=["POST"])
+def create_user():
+
+    data = user_schema.load(request.json)
+
+    user = User(
+        name=data["name"],
+        age=data["age"],
+        email=data["email"]
+    )
+
+    db.session.add(user)
+    db.session.commit()
+
+    return user_schema.dump(user), 201
+
+@app.route("/users", methods=["GET"])
+def get_users():
+
+    user = User.query.all()
+
+    return users_schema.dump(users)
+
+
+@app.route("/users/<int:id>", methods=["GET"])
+def get_user(id):
+
+    user = User.query.get(id)
+
+    if not user:
+        return {"error": "User not found"}, 404
+
+    return user_schema.dump(user)
+
+@app.route("/users/<int:id>", methods=["PUT"])
+def update_user(id):
+
+    user = User.query.get(id)
+
+    if not user:
+        return {"error": "User not found"}, 404
+
+    data = user_schema.load(request.json)
+
+    user.name = data["name"]
+    user.age = data["age"]
+    user.email = data["email"]
+
+    db.session.commit()
+
+    return user_schema.dump(user)
+
+@app.route("/users/<int:id>", methods=["DELETE"])
+def delete_user(id):
+
+    user = User.query.get(id)
+
+    if not user:
+        return {"error": "User not found"}, 404
+
+    db.session.delete(user)
+    db.session.commit()
+
+    return {"message": "User deleted"}
