769fc777747b7c93fa39252f34cc3a54e00af7ec
akssayy <waghralakshay@gmail.com>
2026-03-09 14:14:52 +0530

    connecting database

--- user_api_project/app.py
+++ user_api_project/app.py
@@ -1,19 +1,19 @@
-from flask import Flask 
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://users.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
-
-import routes 
 
 from models import User
 
 with app.app_context():
     db.create_all()
 
+import routes
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True)   
