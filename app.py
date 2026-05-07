764d61713314866e9f624a45185159850db83aff
akssayy <waghralakshay@gmail.com>
2026-05-07 08:37:59 +0530

    CRUD correct code

--- app.py
+++ app.py
@@ -1,32 +1,36 @@
 import sqlite3
 
-conn = sqlite3.connect("collage.db")
-
+conn = sqlite3.connect("college.db")
 cursor = conn.cursor()
 
-"""cursor.execute(CREATE TABLE collage 
-(
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS collage (
     id INTEGER PRIMARY KEY,
-    name TXT,
-    subject TXT
+    name TEXT,
+    subject TEXT
 )
-)
-"""
+""")
+
 cursor.executemany(
-    "INSERT INTO collage (name, subject) VALUES( ?, ?)",
+    "INSERT INTO collage (name, subject) VALUES (?, ?)",
     [
         ("Rahul", "Math"),
         ("Sneha", "Science"),
-        ("Amit","English")
+        ("Amit", "English")
     ]
 )
 
 conn.commit()
+
 cursor.execute("SELECT * FROM collage WHERE subject = 'Math'")
-cursor.execute( "UPDATE collage SET subject = 'Physics' WHERE name = 'Rahul'")
-cursor.execute("DELETE FROM students WHERE name = 'Amit'")
+print("Math students:", cursor.fetchall())
+
+cursor.execute("UPDATE collage SET subject = 'Physics' WHERE name = 'Rahul'")
+cursor.execute("DELETE FROM collage WHERE name = 'Amit'")
+
+conn.commit()
+
 cursor.execute("SELECT * FROM collage")
-print(cursor.fetchall())
+print("All students:", cursor.fetchall())
 
 conn.close()
-
