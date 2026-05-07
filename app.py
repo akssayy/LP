ef782a8433893ea2ab2ac497750f54a1dee67589
akssayy <waghralakshay@gmail.com>
2026-05-07 06:03:01 +0530

    sql 1st

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 import sqlite3
 conn = sqlite3.connect("students.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE students (
+    id INTEGER PRIMARY KEY,
+    name TXT,
+    age INTEGER
+)
+""")
+
+cursor.execute("INSERT INTO students (name, age) VALUES (?, ?)", ("Akshay", 22))
 print("Database created")
 
+conn.commit()
+
+cursor.execute("SELECT * FROM students")
+print(cursor.fetchall())
 conn.close()
