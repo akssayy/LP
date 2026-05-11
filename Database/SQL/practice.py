97dc733d66dfadbd72256fa63ed7ae0cf2cd53b7
akssayy <waghralakshay@gmail.com>
2026-05-11 12:10:32 +0530

    update

--- Database/SQL/practice.py
+++ Database/SQL/practice.py
@@ -4,22 +4,22 @@ conn = sqlite3.connect("collage.db")
 
 cursor = conn.cursor()
 
-cursor.execute("""CREATE TABLE students 
+cursor.execute("""CREATE TABLE emp
 (
     id INTEGER PRIMARY KEY,
     name TXT,
     subject TXT
 )
 """)
-cursor.execute(
-    "INSERT INTO collage (name, subject) VALUES( ?, ?)",
+cursor.executemany(
+    "INSERT INTO emp (name, subject) VALUES( ?, ?)",
     ("Rahul", "Math"),
     ("Sneha", "Science"),
     ("Amit","English")
 )
 
 conn.commit()
-cursor.execute("SELECT & FROM collage")
+cursor.execute("SELECT & FROM emp")
 print(cursor.fetchall())
 
 conn.close()
