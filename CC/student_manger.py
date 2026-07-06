8e77f7eb03dfe44205204091971bf5d6b5470db5
akssayy <waghralakshay@gmail.com>
2026-07-06 18:25:09 +0530

    function pratice

--- CC/student_manger.py
+++ CC/student_manger.py
@@ -1,7 +1,24 @@
 students = []
 
 def add_student(name):
-    return students.append(name)
+    students.append(name)
 
 add_student("Akshay")
+add_student("vijay")
 print(students)
+
+def remove_student(name):
+    if name in students:
+         students.remove(name)
+    else:
+        print("student not found")
+
+remove_student("Akshay")
+print(students)
+
+def show_student():
+    for index, student in enumerate(students, start=1):
+        print(index, student)
+
+    
+show_student()
