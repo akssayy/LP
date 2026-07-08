85ed445b42a5dea356600b6f80a839326103f453
akssayy <waghralakshay@gmail.com>
2026-07-08 19:12:21 +0530

    return in function

--- CC/student_manger.py
+++ CC/student_manger.py
@@ -1,14 +1,16 @@
 marks = [60, 70, 80]
 
 def add_bonus(marks, bonus):
+    new_list = []
     for mark in marks:
         updated_marks = mark + bonus
-        print(updated_marks)
-        
-    return marks.append(updated_marks) 
+        new_list.append(updated_marks) 
 
+    return new_list
 
 print(marks)
-add_bonus(marks, 5)
+print(add_bonus(marks, 5))
+print(add_bonus(marks,10))
+
 
 
