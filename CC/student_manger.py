85ca7b41ace7c730832e8d72d335dd28b48b7d37
akssayy <waghralakshay@gmail.com>
2026-07-09 16:53:53 +0530

    expenses tracker

--- CC/student_manger.py
+++ CC/student_manger.py
@@ -1,16 +1,53 @@
-marks = [60, 70, 80]
+expenses = {
+    "Food": 6000,
+    "Rent": 5500
+}
 
-def add_bonus(marks, bonus):
-    new_list = []
-    for mark in marks:
-        updated_marks = mark + bonus
-        new_list.append(updated_marks) 
+def add_expense(category, amount):
+        if category in expenses:
+            expenses[category] += amount 
+        else:
+            expenses[category] = amount      
+        
+        return expenses 
 
-    return new_list
+def remove_expense(category):
+    if category in expenses:
+        expenses.pop(category)
+    else:
+        print("Category not found.")
+    return expenses
 
-print(marks)
-print(add_bonus(marks, 5))
-print(add_bonus(marks,10))
+def total_expense():
+    return sum(expenses.values())
+
+def highest_expense():
+    i = 0
+    for category, expense in expenses.items():
+        if expense > i:
+            i = expense
+            highest_category = category
+    
+    return (highest_category, i)
+
+
+def show_expenses():
+    for index,category, expense in enumerate(expenses.items(), start=1):
+        print(f"{index}.{category} {expense}")
+    
+    return expenses
 
 
 
+result = add_expense("petrol", 1000)
+print(result)
+
+del_item = remove_expense("pocket money")
+print(del_item)
+
+total = total_expense()
+print(total)
+
+expensive = highest_expense()
+print(expensive)
+show_expenses()
