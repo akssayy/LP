00e1472ffb2cbf9182b54dd668856b3eb03d7454
akssayy <waghralakshay@gmail.com>
2026-07-10 18:20:48 +0530

    calculator

--- CC/student_manger.py
+++ CC/student_manger.py
@@ -1,27 +1,34 @@
-try:
-    num1 = int(input("enter a number: "))
-    num2 = int(input("enter a number: "))
-except ValueError:
-    print("Please enter a number only")
+while True:
+    try:
+        num1 = int(input("enter a number: "))
+        num2 = int(input("enter a number: "))
+        break
+    except ValueError:
+        print("Please enter a number only")
 
 
 user_choice = input("enter action you want to perform, +, -, *, / : ")
 
+
 def calculator(num1, num2, user_choice):
-    if user_choice == "+":
-        return num1 + num2
+    try:
+        if user_choice == "+":
+            return num1 + num2
+        
+        elif user_choice == "-":
+            return num1 - num2
+        
+        elif user_choice == "*":
+            return num1 * num2
+        
+        elif user_choice == "/":
+                return num1 / num2
+        
+        else:
+            return("invalid choice")
+    except ZeroDivisionError:
+        return("can't divide by zero")
     
-    elif user_choice == "-":
-        return num1 - num2
-    
-    elif user_choice == "*":
-        return num1 * num2
-
-    elif user_choice == "/":
-        return num1 / num2
-
-    else:
-        print("invalid choice")
 
 result = calculator(num1, num2, user_choice)
 print(result)
