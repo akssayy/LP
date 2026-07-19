07a0dab4ecae8dbf382445ab39bb62bf2cc18c0e
akssayy <waghralakshay@gmail.com>
2026-07-19 19:05:54 +0530

    flask

--- expense_api/app.py
+++ expense_api/app.py
@@ -11,13 +11,31 @@ expenses = [
 def get_expense():
     return jsonify(expenses)
 
-@app.route("/add", methods=["POST"])
+@app.route("/expenses", methods=["POST"])
 def add_expense():
-    data = request.get_json
+    data = request.get_json()
+
+    if not data:
+        return jsonify({"error": "Invalid or missing data"})
 
     category = data.get("category")
     amount = data.get("amount")
 
-    if not category:
-        return jsonify-
+    if category is None:
+        return jsonify({"error":"category cannot be empty"}, {400: "bad request"})
+    
+    if amount < 0:
+        return jsonify({"error": "amount must be greater than 0"},{400: "bad request"})
+
+    new_expenses = {
+        "category": category,
+        "amount": amount
+    }
+
+    expenses.append(new_expenses)
+
+    return jsonify(expenses),201
+    
+
+
 app.run(debug=True)
